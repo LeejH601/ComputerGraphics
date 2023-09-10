9b5534b1c93350ecc0da08a0535e91085dcff8c4
LeejH601 <77761502+LeejH601@users.noreply.github.com>
2023-09-10 15:12:36 +0900

    중간 지점 렌더링 변경, 장애물 추가

--- CumGuWarmingUp/CumGuWarmingUp/warming7.cpp
+++ CumGuWarmingUp/CumGuWarmingUp/warming7.cpp
@@ -30,12 +30,134 @@ struct Point {
 
 };
 
+struct MYRECT {
+	Point leftTop;
+	Point rightBottom;
+
+	MYRECT() = default;
+	MYRECT(Point p1, Point p2) : leftTop{ p1 }, rightBottom{ p2 } {};
+
+	void Render() {
+		if (leftTop.x == -1)
+			return;
+		for (int i = leftTop.x; i <= rightBottom.x; ++i) {
+			for (int j = leftTop.y; j <= rightBottom.y; ++j) {
+				int x = i;
+				int y = j;
+
+				x %= MAX_BOARD_SIZE;
+				y %= MAX_BOARD_SIZE;
+
+				gotoxy(x * BOARD_OFFSET, y * BOARD_OFFSET / 2);
+				std::cout << std::string("бс");
+			}
+		}
+	}
+
+	void update() {
+		if (leftTop.x >= MAX_BOARD_SIZE) {
+			leftTop.x -= MAX_BOARD_SIZE;
+			rightBottom.x -= MAX_BOARD_SIZE;
+		}
+		if (leftTop.x < 0) {
+			leftTop.x += MAX_BOARD_SIZE;
+			rightBottom.x += MAX_BOARD_SIZE;
+		}
+		if (leftTop.y < 0) {
+			leftTop.y += MAX_BOARD_SIZE;
+			rightBottom.y += MAX_BOARD_SIZE;
+		}
+
+		if (leftTop.y > MAX_BOARD_SIZE) {
+			leftTop.y -= MAX_BOARD_SIZE;
+			rightBottom.y -= MAX_BOARD_SIZE;
+		}
+	}
+
+	void moveX(bool dir) {
+		if (dir) {
+			leftTop.x++;
+			rightBottom.x++;
+
+
+		}
+		else {
+			leftTop.x--;
+			rightBottom.x--;
+
+
+		}
+	}
+
+	void moveY(bool dir) {
+		if (dir) {
+			leftTop.y--;
+			rightBottom.y--;
+
+
+		}
+		else {
+			leftTop.y++;
+			rightBottom.y++;
+
+		}
+	}
+
+	void scale(bool isBig) {
+		if (isBig) {
+			scaleX(true);
+			scaleY(true);
+		}
+		else {
+			scaleX(false);
+			scaleY(false);
+		}
+	}
+
+	void scaleX(bool isBig) {
+		if (isBig) {
+			leftTop.x--;
+			rightBottom.x++;
+		}
+		else {
+			if (rightBottom.x - leftTop.x >= 2) {
+				leftTop.x++;
+				rightBottom.x--;
+			}
+			else if (rightBottom.x - leftTop.x == 1) {
+				rightBottom.x--;
+			}
+		}
+	}
+
+	void scaleY(bool isBig) {
+		if (isBig) {
+			leftTop.y--;
+			rightBottom.y++;
+		}
+		else {
+			if (rightBottom.y - leftTop.y >= 2) {
+				leftTop.y++;
+				rightBottom.y--;
+			}
+			else if (rightBottom.y - leftTop.y == 1) {
+				rightBottom.y--;
+			}
+		}
+	}
+};
+
 int board[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
 std::vector<Point> Route;
+std::vector<MYRECT> Obstacles;
+std::vector<Point> GoalList;
 std::random_device rd;
 std::default_random_engine dre(rd());
 std::uniform_int_distribution<int> uid(0, 3);
-std::uniform_int_distribution<int> uid_point(0, MAX_BOARD_SIZE - 1);
+std::uniform_int_distribution<int> uid_obscount(5, 10);
+std::uniform_int_distribution<int> uid_obs(2, MAX_BOARD_SIZE - 8);
+std::uniform_int_distribution<int> uid_obsSize(1, 4);
+std::uniform_int_distribution<int> uid_point(1, MAX_BOARD_SIZE - 2);
 
 std::map<int, int> RouteDirectionMap{};
 
@@ -45,7 +167,6 @@ float distance(Point p1, Point p2) {
 		pow(abs(p1.x - p2.x), 2) + pow(abs(p1.y - p2.y), 2)
 	);
 }
-
 
 class NODE {
 	friend Point operator+(const NODE& lhs, const NODE& rhs);
@@ -100,6 +221,22 @@ bool CheckPointVaild(const Point& p) {
 		return false;
 	if (p.y >= MAX_BOARD_SIZE)
 		return false;
+
+	/*POINT P;
+	P.x = p.x;
+	P.y = p.y;
+
+	for (MYRECT& obs : Obstacles) {
+		RECT rect;
+		rect.left = obs.leftTop.x;
+		rect.top = obs.leftTop.y;
+		rect.right = obs.rightBottom.x;
+		rect.bottom = obs.rightBottom.y;
+
+		if (PtInRect(&rect, P))
+			return false;
+	}*/
+
 	return true;
 }
 
@@ -115,12 +252,22 @@ Point operator+(const Point& lhs, const Point& rhs) {
 	return Point(lhs.x + rhs.x, lhs.y + rhs.y);
 }
 
-void makeRouteAStar() {
+bool makeRouteAStar() {
+	Obstacles.clear();
+	int nObstacles = uid_obscount(dre);
+	for (int i = 0; i < nObstacles; ++i) {
+		Point p{ uid_obs(dre),uid_obs(dre) };
+		Point size{ uid_obsSize(dre),uid_obsSize(dre) };
+		MYRECT rect{ p, Point(p.x + size.x, p.y + size.y)};
+
+		Obstacles.emplace_back(rect);
+	}
+
 	Route.clear();
 
 	static std::uniform_int_distribution<int> uid_goals(4, 20);
 
-	std::vector<Point> GoalList;
+	GoalList.clear();
 	int nGoals = uid_goals(dre);
 	GoalList.emplace_back(0, 0);
 	for (int i = 1; i < nGoals - 1; ++i) {
@@ -128,10 +275,18 @@ void makeRouteAStar() {
 	}
 	GoalList.emplace_back(MAX_BOARD_SIZE - 1, MAX_BOARD_SIZE - 1);
 
+	std::vector<NODE> globalClosed;
+	for (MYRECT& obs : Obstacles) {
+		for (int i = obs.leftTop.x; i <= obs.rightBottom.x; ++i) {
+			for (int j = obs.leftTop.y; j <= obs.rightBottom.y; ++j) {
+				globalClosed.emplace_back(Point(i, j), FLT_MAX, FLT_MAX, Point(-1, -1));
+			}
+		}
+	}
 
 	for (int i = 0; i < nGoals - 1; ++i) {
 		std::list<NODE> OpenNodes;
-		std::vector<NODE> CloseNodes;
+		std::vector<NODE> CloseNodes = globalClosed;
 
 		Point startPoint = GoalList[i];
 		Point nextGoal = GoalList[i + 1];
@@ -140,6 +295,8 @@ void makeRouteAStar() {
 
 		while (true)
 		{
+			if (OpenNodes.empty())
+				return false;
 			auto node_it = OpenNodes.begin();
 			for (auto it = OpenNodes.begin(); it != OpenNodes.end(); ++it) {
 				if (node_it->GetFScore() > it->GetFScore())
@@ -207,7 +364,7 @@ void makeRouteAStar() {
 		}
 	}
 
-	return;
+	return true;
 }
 
 
@@ -257,6 +414,14 @@ void makeRoute() {
 	}
 }
 
+void RenderGoals() {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+	for (Point& p : GoalList) {
+		gotoxy(p.x * BOARD_OFFSET, p.y * BOARD_OFFSET / 2);
+		std::cout << std::string("бс");
+	}
+}
+
 void RenderBoard() {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 	for (int i = 0; i < MAX_BOARD_SIZE; ++i) {
@@ -264,6 +429,10 @@ void RenderBoard() {
 			gotoxy(i * BOARD_OFFSET, j * BOARD_OFFSET / 2);
 			std::cout << std::string("бс");
 		}
+	}
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+	for (MYRECT& rect : Obstacles) {
+		rect.Render();
 	}
 }
 
@@ -285,10 +454,20 @@ int main() {
 	Point OldPlayerPos = player;
 	int nPlayerIndex = -1;
 
+	auto CreateRoute = []() {
+		while (true)
+		{
+			if (makeRouteAStar())
+				break;
+		}
+	};
+
 	//makeRoute();
-	makeRouteAStar();
+	
+	CreateRoute();
 	RenderBoard();
 	RenderRoute();
+	RenderGoals();
 
 	auto RenderPlayer = [&]() {
 		if (nPlayerIndex == -1)
@@ -302,7 +481,7 @@ int main() {
 		player.y = Route[nPlayerIndex].y;
 
 		gotoxy(player.x * BOARD_OFFSET, player.y * BOARD_OFFSET / 2);
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
 		std::cout << std::string("б┌");
 
 		OldPlayerPos = player;
@@ -322,10 +501,11 @@ int main() {
 			RouteDirectionMap[2] = 0;
 			RouteDirectionMap[3] = 0;
 
-			makeRouteAStar();
+			CreateRoute();
 
 			RenderBoard();
 			RenderRoute();
+			RenderGoals();
 
 			player.x = -1;
 			player.y = -1;
@@ -353,13 +533,14 @@ int main() {
 				break;
 			}
 		}
-			break;
+		break;
 		case 'q':
 			return 0;
 		default:
 			break;
 		}
 
+		RenderGoals();
 		RenderPlayer();
 	}
 
